394cbbfeac3f1b1066be042ad92f3fb4f0f4b830
KurataYuki-fjsw <151592104+KurataYuki-fjsw@users.noreply.github.com>
2024-01-16 15:37:42 +0900

    フォルダを追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 31  //1以上の正の数
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+else {
+  console.log(num);
+}
