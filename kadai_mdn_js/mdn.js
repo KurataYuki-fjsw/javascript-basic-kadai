813ac05c72e4698dfee284415891df2eb4cb1bcc
KurataYuki-fjsw <151592104+KurataYuki-fjsw@users.noreply.github.com>
2024-01-17 14:28:28 +0900

    フォルダを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const date = today.getDate();
+
+console.log(year);
+console.log(month);
+console.log(date);
+
+console.log(year + '年'+ month+1 + '月' + date + '日');
